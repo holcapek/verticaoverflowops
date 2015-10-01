27606a06ac6b308b624426c0f61fc9bbd0a3f6ce
Jan Holcapek <holcapek@gmail.com>
2015-10-01 16:03:55 +0200

    Handle over- and underflow on adding

--- overflowops.cpp
+++ overflowops.cpp
@@ -3,8 +3,11 @@
 using namespace Vertica;
 using namespace std;
 
+#define SAMESIGN(a,b) (((a)<0)==((b)<0))
+
 class OverflowAdd : public ScalarFunction
 {
+
 public:
 virtual void
 processBlock(
@@ -18,8 +21,13 @@ processBlock(
 		const vint a = arg_reader.getIntRef(0);
 		const vint b = arg_reader.getIntRef(1);
 		const vint c = a + b;
-		if (c < a || c < b)
-			vt_report_error(0, "Addition overflow.");
+
+		// https://github.com/postgres/postgres/blob/aa9eac45ea868e6ddabc4eb076d18be10ce84c6a/src/backend/utils/adt/int8.c#L513
+
+		// TODO handle c == vint_null
+		if (SAMESIGN(a, b) && !SAMESIGN(c, a))
+			vt_report_error(0, "vint out of range");
+
 		res_writer.setInt(c);
 		res_writer.next();
 	}
